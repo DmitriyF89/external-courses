38d87593561351c79a38e14c01a902fa73aa2068
DmitriyF <5thgrade123@gmail.com>
2019-11-19 00:42:25 +0300

    refactor: rework Hangman using ES5 function constructor syntax

--- src/ex14_js-context/task-02.js
+++ src/ex14_js-context/task-02.js
@@ -1,66 +1,65 @@
-class Hangman {
-  constructor(word) {
-    this.word = word;
-    this.letters = this.word.split('');
-    this.playField = this.letters.map(() => '_');
-    this.attempts = 6;
-    this.wrongGuesses = [];
-    this.count = 0;
+function Hangman(word) {
+  this.word = word;
+  this.letters = this.word.split('');
+  this.playField = this.letters.map(() => '_');
+  this.attempts = 6;
+  this.wrongGuesses = [];
+  this.count = 0;
+}
+
+Hangman.prototype.getErrorsLeft = function () {
+  return this.attempts;
+}
+
+Hangman.prototype.getWrongSymbols = function () {
+  return this.wrongGuesses;
+}
+
+Hangman.prototype.getStatus = function () {
+  return `${this.playField.join('')} | errors left ${this.attempts}`;
+}
+
+Hangman.prototype.startAgain = function (word) {
+  this.word = word;
+  this.letters = this.word.split('');
+  this.playField = this.letters.map(() => '_');
+  this.attempts = 6;
+  this.wrongGuesses = [];
+  this.count = 0;
+
+  return this;
+}
+
+Hangman.prototype.getGuessedString = function () {
+  return this.playField.join('');
+};
+
+Hangman.prototype.guess = function (char) {
+  if (this.attempts < 1) {
+    console.log('You lost =(');
+    return this;
   }
 
-  getErrorsLeft() {
-    return this.attempts;
-  };
-
-  getWrongSymbols() {
-    return this.wrongGuesses;
-  };
-
-  getStatus() {
-    return `${this.playField.join('')} | errors left ${this.attempts}`;
-  };
-
-  startAgain(word) {
-    this.word = word;
-    this.letters = this.word.split('');
-    this.playField = this.letters.map(() => '_');
-    this.attempts = 6;
-    this.wrongGuesses = [];
-    this.count = 0;
-
-    return this;
-  };
-
-  getGuessedString() {
-    return this.playField.join('');
-  };
-
-  guess(char) {
-    if (this.attempts >= 1) {
-      if (this.letters.includes(char)) {
-        this.letters.forEach((element, index) => {
-          if (element === char) {
-            this.playField[index] = char;
-            this.count++;
-          }
-        });
-
-        if (this.count === this.word.length) {
-          console.log(`${this.playField.join('')} | You won!`);
-        } else {
-          console.log(this.playField.join(''));
-        }
-
-      } else {
-        this.wrongGuesses.push(char);
-        this.attempts--;
-        console.log(`Wrong letter, errors left ${this.attempts} | ${this.wrongGuesses.join(',')}`)
+  if (this.letters.includes(char)) {
+    this.letters.forEach((element, index) => {
+      if (element === char) {
+        this.playField[index] = char;
+        this.count++;
       }
+    });
+
+    if (this.count === this.word.length) {
+      console.log(`${this.playField.join('')} | You won!`);
     } else {
-      console.log('You lost =(');
+      console.log(this.playField.join(''));
     }
-    return this;
+
+  } else {
+    this.wrongGuesses.push(char);
+    this.attempts--;
+    console.log(`Wrong letter, errors left ${this.attempts} | ${this.wrongGuesses.join(',')}`)
   }
+  return this;
 }
 
 const hangman = new Hangman('webpurple');
